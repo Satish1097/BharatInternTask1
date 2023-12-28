61ded9c8f4308824a9e9b9fbdb40a169965771f1
SATISH KUMAR <138617607+Satish1097@users.noreply.github.com>
2023-12-29 00:03:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function openmenu()
+{
+    let x=document.getElementById("menu");
+    x.style.left="0%"
+}
+function closemenu()
+{
+    let y=document.getElementById("menu");
+    y.style.left="-100%"
+}
+function project1(){
+    window.location="https://satish1097.github.io/Codsoft1/";
+}
+function project2(){
+    window.location="https://satish1097.github.io/Amazon-clone/";
+}
+function project3(){
+    window.location=" https://satish1097.github.io/Air_GuardianoftheSky/";
+}
+function hire(){
+    window.location="https://www.linkedin.com/in/satish-kumar-b28b8b275/";
+}
